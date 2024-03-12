453b580cc8e19a19658a4317239760163cd7a164
AugustoUrquizu <augusto.urquizu@gmail.com>
2024-03-12 16:49:49 -0300

    Cambios

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,5 +1,15 @@
-from django.urls import path
+from django.urls import path, re_path, include
 from . import views
-from usuarios.views import registrar
-urlpatterns = [ path("registrar/", registrar, name='registrar')
+from usuarios.views import registrar, login, perfil, editar_perfil, EditarContrasenia
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
+
+
+
+urlpatterns = [ path("registrar/", registrar, name='registrar'), 
+               path("login/", login, name="login"),
+               path("logout/", LogoutView.as_view(template_name="logout.html"), name="logout"), 
+               path("perfil/", perfil, name='perfil'), 
+               path("editar_perfil/", editar_perfil, name="editar_perfil"),
+               path('cambiar_contrasenia/', views.EditarContrasenia.as_view(), name='cambiar_contrasenia'),
 ]
