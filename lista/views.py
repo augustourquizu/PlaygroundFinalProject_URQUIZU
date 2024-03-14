6a43cb7869b509a28ea132513800b9046707af92
AugustoUrquizu <augusto.urquizu@gmail.com>
2024-03-14 20:56:14 -0300

    Terminado esta vez si

--- lista/views.py
+++ lista/views.py
@@ -13,27 +13,27 @@ def acerca_de_mi(request):
 class Evento(ListView):
     model = Lista
     context_object_name = 'evento'
-    template_name = "lista/evento.html"
+    template_name = "eventos/evento.html"
 
     
 class CrearElemento(LoginRequiredMixin, CreateView):
     model = Lista
-    template_name = "lista/crear_elemento.html"
+    template_name = "eventos/crear_elemento.html"
     fields = [ 'nombre','descripcion', 'dia', 'imagen', 'lugar']
     success_url = reverse_lazy('evento')
 
 class EliminarElemento(LoginRequiredMixin, DeleteView):
     model = Lista
-    template_name = "lista/eliminar_elemento.html"
+    template_name = "eventos/eliminar_elemento.html"
     success_url = reverse_lazy('evento')
 
 class EditarElemento(LoginRequiredMixin, UpdateView):
     model = Lista
-    template_name = "lista/editar_elemento.html"
+    template_name = "eventos/editar_elemento.html"
     success_url = reverse_lazy('evento')
     fields = [ 'nombre', 'descripcion', 'dia', 'imagen', 'lugar']
 
 class DetalleElemento(DetailView):
     model = Lista
-    template_name = "lista/detalle_elemento.html"
+    template_name = "eventos/detalle_elemento.html"
 # Create your views here.
